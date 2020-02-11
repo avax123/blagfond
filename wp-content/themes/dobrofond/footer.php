e643d83517458ef5f111c1c185002187da8a8acd
mikhailKudriavtsev <mikhail.kudriavtsev@catenamedia.com>
2020-02-11 17:56:47 +0200

    ins content to header footer index, add styles

---
+++ wp-content/themes/dobrofond/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="container">
+            <ul>
+                <li><a class="icon-soc1" href="https://www.ok.ru/bfputevkavzhizn"  target="_blank">Одноклассники</a></li>
+                <li><a class="icon-soc2" href="https://vk.com/pvz2016" target="_blank">VKontakte</a></li>
+                <li><a class="icon-soc3" href="https://www.facebook.com/pvz2016" target="_blank">Facebook</a></li>
+                <li><a class="icon-soc4" href="https://www.instagram.com/pvz_2016/" target="_blank">Instagram</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+<script src="<?php echo get_stylesheet_directory_uri().'/js/all-plugins.min.js' ?>"></script>
+<script src="<?php echo get_stylesheet_directory_uri().'/js/main.js' ?>">
+</div>
+<script src="<?php echo get_stylesheet_directory_uri().'/js/video.js' ?>"></script>
+</body>
+</html>
---
+++ wp-content/themes/dobrofond/header.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="format-detection" content="telephone=no">
+    <title>Благотворительный фонд Путевка в жизнь</title>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(39939355, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+            trackHash: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/39939355" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-85037271-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-85037271-1');
+    </script>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri().'/css/style.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri().'/css/all-plugins.css' ?>">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700,800&amp;display=swap&amp;subset=cyrillic" rel="stylesheet">
+    <link href="<?php echo get_stylesheet_directory_uri().'/css/video-js.css' ?>" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <div class="background-block">
+        <header class="header">
+            <div class="top-header">
+                <div class="container">
+                    <div class="flex-wrap between-xs">
+                        <div class="top-header__left">
+                            <ul class="header-nav header-nav--visible-sm">
+                                <li><a href="index.html">Стать стипендиатом</a></li>
+                            </ul>
+                            <ul class="header-nav">
+                                <li><a href="about-us.html">О благотворительном фонде</a></li>
+                                <li><a href="publik.pdf" target="_blank">Публичная отчетность</a></li>
+                                <li><a href="#">Блог фонда</a></li>
+                            </ul>
+                        </div>
+                        <div class="top-header__right">
+                            <ul class="header-nav">
+                                <li><a class="phone-block" href="tel:+79858478945"><i class="icon-phone"></i>+7 (985) 847-89-45</a></li>
+                                <li><a href="contacts.html">Контакты и реквизиты</a></li>
+                            </ul>
+                        </div>
+                        <div class="mob-button"><span></span><span></span><span></span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="middle-header">
+                <div class="container">
+                    <div class="flex-wrap between-xs middle-xs">
+                        <div class="logo"><a href="index.html"><img src="images/logo.svg" alt=""></a></div>
+                        <a class="btn btn--white" href="donats.html">Я хочу помочь</a>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-header">
+                <div class="container">
+                    <ul class="header-nav">
+                        <li><a href="index.html">Стать стипендиатом</a></li>
+                        <li><a href="stepend.html">Стипендиаты</a></li>
+                        <li><a href="smi.html">СМИ о стипендии</a></li>
+                        <li><a href="volonteri.html">Нам помогают</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="mobile-nav-drop"></div>
+            <div class="mobile-nav">
+                <div class="mobile-nav-over">
+                    <div class="mobile-nav-group">
+                        <ul>
+                            <li><a href="about-us.html">О благотворительном фонде</a></li>
+                            <li><a href="publik.pdf" target="_blank">Публичная отчетность</a></li>
+                            <li><a href="#">Блог фонда</a></li>
+                            <li><a href="contacts.html">Контакты и реквизиты</a></li>
+                        </ul>
+                    </div>
+                    <div class="mobile-nav-group">
+                        <ul>
+                            <li><a href="index.html">Стать стипендиатом</a></li>
+                            <li><a href="stepend.html">Стипендиаты</a></li>
+                            <li><a href="smi.html">СМИ о стипендии</a></li>
+                            <li><a href="volonteri.html">Нам помогают</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="mobile-nav-butt"><a class="btn btn--white" href="donats.html">Я хочу помочь</a></div>
+            </div>
+        </header>
